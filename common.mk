808b49f1a95c02021afeefc4b6416662be71c88b
opensourcefreak <john@snurr.nu>
2025-05-11 14:36:02 +0200

    exynos9810: build libion.vendor

--- common.mk
+++ common.mk
@@ -86,7 +86,8 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.composer@2.2-service \
-    android.hardware.graphics.mapper@2.0-impl-2.1
+    android.hardware.graphics.mapper@2.0-impl-2.1 \
+    libion.vendor
 
 # Doze
 PRODUCT_PACKAGES += \
